13d708411d8ae9f2545c52ee6001171d56d61c7e
ajdinl <ajdinl@live.com>
2024-01-12 14:11:35 +0100

    dashboard component update

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -3,8 +3,12 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '@/utils/supabaseClient'
 export default function DashboardComponent() {
-  const [userId, setUserId] = useState(null)
+  const [user, setUser] = useState(null)
   const [restaurant, setRestaurant] = useState(null)
+  const [restaurants, setRestaurants] = useState([])
+
+  const isAdmin = user?.user?.user_metadata.is_admin
+  const userId = user?.user?.id
 
   const getUser = async () => {
     try {
@@ -13,9 +17,7 @@ export default function DashboardComponent() {
         console.error('Error fetching user:', error)
         return
       }
-
-      const userId = data.user.id
-      setUserId(userId)
+      setUser(data)
     } catch (error) {
       console.error('Error fetching user:', error)
     }
@@ -23,9 +25,25 @@ export default function DashboardComponent() {
 
   const fetchRestaurant = async () => {
     try {
+      if (isAdmin) {
+        const { data, error } = await supabase
+          .from('restaurant')
+          .select(
+            '*, restaurant_menu(*), restaurant_orders(*), restaurant_table(*)'
+          )
+        if (error) {
+          console.error('Error fetching restaurant:', error)
+          return
+        }
+        setRestaurants(data)
+        return
+      }
+
       const { data, error } = await supabase
         .from('restaurant')
-        .select('*, restaurant_menu(*)')
+        .select(
+          '*, restaurant_menu(*), restaurant_orders(*), restaurant_table(*)'
+        )
         .eq('user_id', userId)
         .single()
 
@@ -45,19 +63,26 @@ export default function DashboardComponent() {
   }, [])
 
   useEffect(() => {
-    if (userId) {
+    if (user) {
       fetchRestaurant()
     }
-  }, [userId])
+  }, [user])
 
   return (
     <div className='flex flex-col items-center justify-center bg-white min-h-screen w-full'>
       <h1>Dashboard</h1>
       <div className='bg-gray-200 rounded-lg shadow-lg p-6 w-full max-w-lg text-center'>
-        {restaurant && <h1 className='text-3xl'>{restaurant.name}</h1>}
+        {isAdmin &&
+          restaurants.map((restaurant) => (
+            <div key={restaurant.id}>
+              <h1 className='text-3xl'>{restaurant.name}</h1>
+              <p>{restaurant.name}</p>
+            </div>
+          ))}
         {restaurant &&
           restaurant?.restaurant_menu.map((menu) => (
             <div key={menu.id} className='my-4'>
+              {restaurant && <h1 className='text-3xl'>{restaurant.name}</h1>}
               <ul>
                 {menu.items.map((item, index) => (
                   <li key={index}>{item}</li>
