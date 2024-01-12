49498d41cf588103d9708d6baee13287c3ba6710
ajdinl <ajdinl@live.com>
2024-01-12 14:28:31 +0100

    update dashboard component

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,13 +4,11 @@ import { useState, useEffect } from 'react'
 import { supabase } from '@/utils/supabaseClient'
 export default function DashboardComponent() {
   const [user, setUser] = useState(null)
-  const [restaurant, setRestaurant] = useState(null)
-  const [restaurants, setRestaurants] = useState([])
-
+  const [data, setData] = useState([])
   const isAdmin = user?.user?.user_metadata.is_admin
   const userId = user?.user?.id
 
-  const getUser = async () => {
+  const fetchUserData = async () => {
     try {
       const { data, error } = await supabase.auth.getUser()
       if (error) {
@@ -23,48 +21,38 @@ export default function DashboardComponent() {
     }
   }
 
-  const fetchRestaurant = async () => {
+  const fetchRestaurantData = async () => {
     try {
-      if (isAdmin) {
-        const { data, error } = await supabase
-          .from('restaurant')
-          .select(
-            '*, restaurant_menu(*), restaurant_orders(*), restaurant_table(*)'
-          )
-        if (error) {
-          console.error('Error fetching restaurant:', error)
-          return
-        }
-        setRestaurants(data)
-        return
-      }
-
-      const { data, error } = await supabase
+      let query = supabase
         .from('restaurant')
         .select(
           '*, restaurant_menu(*), restaurant_orders(*), restaurant_table(*)'
         )
-        .eq('user_id', userId)
-        .single()
+
+      if (!isAdmin) {
+        query = query.eq('user_id', userId).single()
+      }
+
+      const { data, error } = await query
 
       if (error) {
         console.error('Error fetching restaurant:', error)
         return
       }
 
-      setRestaurant(data)
+      setData(data)
     } catch (error) {
       console.error('Error fetching restaurant:', error)
     }
   }
 
   useEffect(() => {
-    getUser()
+    fetchUserData()
   }, [])
 
   useEffect(() => {
     if (user) {
-      fetchRestaurant()
+      fetchRestaurantData()
     }
   }, [user])
 
@@ -73,16 +61,15 @@ export default function DashboardComponent() {
       <h1>Dashboard</h1>
       <div className='bg-gray-200 rounded-lg shadow-lg p-6 w-full max-w-lg text-center'>
         {isAdmin &&
-          restaurants.map((restaurant) => (
+          data.map((restaurant) => (
             <div key={restaurant.id}>
-              <h1 className='text-3xl'>{restaurant.name}</h1>
               <p>{restaurant.name}</p>
             </div>
           ))}
-        {restaurant &&
-          restaurant?.restaurant_menu.map((menu) => (
+        {data &&
+          data?.restaurant_menu?.map((menu) => (
             <div key={menu.id} className='my-4'>
-              {restaurant && <h1 className='text-3xl'>{restaurant.name}</h1>}
+              <h1 className='text-3xl mb-2'>{data.name}</h1>
               <ul>
                 {menu.items.map((item, index) => (
                   <li key={index}>{item}</li>
