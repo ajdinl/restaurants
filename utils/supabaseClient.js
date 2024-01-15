8d50fe3959cf31b387d7bece8bfeb67ddeb05b0a
ajdinl <ajdinl@live.com>
2024-01-15 20:01:12 +0100

    move functions to utils - supabase file

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -7,4 +7,29 @@ const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 )
 
-export { supabase }
+const getUser = async () => {
+  const { data, error } = await supabase.auth.getUser()
+  return { data, error }
+}
+
+const signInWithPassword = async (email, password) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+  return { data, error }
+}
+
+const fetchRestaurants = async (isAdmin, userId) => {
+  let query = supabase
+    .from('restaurants')
+    .select('*, restaurant_menu(*), restaurant_orders(*), restaurant_tables(*)')
+
+  if (!isAdmin) {
+    query = query.eq('user_id', userId).single()
+  }
+
+  return query
+}
+
+export { supabase, getUser, signInWithPassword, fetchRestaurants }
