c7c71c8fcac31ae08ee23cbe09458db9e4fdf731
ajdinl <ajdinl@live.com>
2024-01-15 20:00:09 +0100

    remove duplicate code and move to Dashboard component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,5 @@
 import DashboardComponent from '@/components/Dashboard'
 
 export default function Dashboard() {
-  return (
-    <div className='flex flex-col items-end bg-white'>
-      <form action='/auth/signout' method='post'>
-        <button
-          type='submit'
-          className='bg-gray-700 text-white font-bold py-2 px-4 rounded hover:bg-gray-500 m-4'
-        >
-          Sign Out
-        </button>
-      </form>
-      <DashboardComponent />
-    </div>
-  )
+  return <DashboardComponent />
 }
