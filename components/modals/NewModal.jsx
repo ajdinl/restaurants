e652e052464095755f01a058b91127a9ac896aed
ajdinl <ajdinl@live.com>
2024-02-01 11:28:33 +0100

    add error display, and add new dish functionality

--- components/modals/NewModal.jsx
+++ components/modals/NewModal.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { addNewReservation } from '@/utils/supabaseMethods'
+import { addNewReservation, updateArrayItem } from '@/utils/supabaseMethods'
 import {
   Card,
   CardHeader,
@@ -20,9 +20,20 @@ export default function NewModal({
   restaurants,
 }) {
   const [table, setTable] = useState({})
+  const [dish, setDish] = useState(null)
+  const [error, setError] = useState('')
+
+  const handleSave = async () => {
+    if (selected.category === 'Reservation') {
+      handleReservationSave()
+    } else if (selected.category === 'Dish') {
+      handleDishSave()
+    }
+  }
 
   const handleReservationSave = async () => {
     if (!table.number || !table.capacity) {
+      setError('Please select field')
       return
     }
     if (!table.restaurant_id) {
@@ -42,6 +53,28 @@ export default function NewModal({
     }
   }
 
+  const handleDishSave = async () => {
+    if (!dish) {
+      setError('Please fill name of the dish')
+      return
+    }
+
+    const selectedArray = [...selected.menu.items]
+    selectedArray.push(dish)
+
+    const { data, error } = await updateArrayItem(
+      'menu',
+      selected.menu.id,
+      selectedArray
+    )
+    if (error) {
+      console.error('Error adding item:', error)
+      return
+    } else {
+      fetchRestaurantsData()
+      setShowNewModal(false)
+    }
+  }
   return (
     <>
       <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
@@ -59,11 +92,13 @@ export default function NewModal({
             <div className='relative p-6 flex-auto'>
               <Card>
                 <CardHeader>
-                  <CardTitle>New {selected}</CardTitle>
-                  <CardDescription>Add a new {selected}.</CardDescription>
+                  <CardTitle>New {selected.category}</CardTitle>
+                  <CardDescription>
+                    Add a new {selected.category}.
+                  </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  {selected === 'Reservation' && (
+                  {selected.category === 'Reservation' && (
                     <form className='space-y-4'>
                       {isAdmin && (
                         <label className='block'>
@@ -87,13 +122,16 @@ export default function NewModal({
                       )}
                       <label className='block'>
                         <span className='text-gray-700'>Table Number</span>
+                        <span className='text-red-500 ml-4 text-sm'>
+                          {!table.number && error}
+                        </span>
                         <select
-                          defaultValue='1'
                           onChange={(e) =>
                             setTable({ ...table, number: e.target.value })
                           }
                           className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
                         >
+                          <option>Select</option>
                           <option>1</option>
                           <option>2</option>
                           <option>3</option>
@@ -108,12 +146,16 @@ export default function NewModal({
                       </label>
                       <label className='block'>
                         <span className='text-gray-700'>Number of Guests</span>
+                        <span className='text-red-500 ml-4 text-sm'>
+                          {!table.capacity && error}
+                        </span>
                         <select
                           onChange={(e) =>
                             setTable({ ...table, capacity: e.target.value })
                           }
                           className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
                         >
+                          <option>Select</option>
                           <option>1</option>
                           <option>2</option>
                           <option>3</option>
@@ -139,6 +181,21 @@ export default function NewModal({
                       </label>
                     </form>
                   )}
+                  {selected.category === 'Dish' && (
+                    <form className='space-y-4'>
+                      <label className='block'>
+                        <span className='text-gray-700'>Name</span>
+                        <span className='text-red-500 ml-4 text-sm'>
+                          {!dish && error}
+                        </span>
+                        <input
+                          type='text'
+                          onChange={(e) => setDish(e.target.value)}
+                          className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                        />
+                      </label>
+                    </form>
+                  )}
                 </CardContent>
               </Card>
             </div>
@@ -153,7 +210,7 @@ export default function NewModal({
               <Button
                 className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type='button'
-                onClick={() => handleReservationSave()}
+                onClick={() => handleSave()}
               >
                 Save Changes
               </Button>
