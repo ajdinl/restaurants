bd9db3b7bd8c23005166b25bc2fb8d7cf70b1768
ajdinl <ajdinl@live.com>
2024-02-01 10:32:16 +0100

    welcome message

--- components/AdminDashboard.jsx
+++ components/AdminDashboard.jsx
@@ -118,6 +118,11 @@ export default function AdminDashboardComponent() {
   return (
     <div>
       <main className='flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10'>
+        {!view && (
+          <div className='text-3xl text-center'>
+            Welcome to the Admin Dashboard
+          </div>
+        )}
         {view === 'restaurants' && (
           <Card>
             <CardHeader>
