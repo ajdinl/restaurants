17a8c92d17d25b5b3c5dd361fef8040afcd7ded6
ajdinl <ajdinl@live.com>
2024-02-06 15:28:48 +0100

    implement error in new modal for orders

--- components/modals/NewModal.jsx
+++ components/modals/NewModal.jsx
@@ -99,6 +99,11 @@ export default function NewModal({
     const { restaurantId, orderNumber } = selected
     const tableNumber = table.table_number
 
+    if (!tableNumber) {
+      setError('Please select table')
+      return
+    }
+
     const { data, error } = await addNewOrder(
       restaurantId,
       tableNumber,
@@ -257,7 +262,7 @@ export default function NewModal({
                     </form>
                   )}
                   {selected.category === 'Order' && (
-                    <form className='space-y-4'>
+                    <form className='flex flex-col space-y-4'>
                       {isAdmin && (
                         <select
                           onChange={(e) =>
@@ -280,8 +285,9 @@ export default function NewModal({
                             )}
                         </select>
                       )}
+                      <span className='text-red-500 text-sm'>{error}</span>
                       <Button
-                        className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                        className='w-20 bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                         onClick={() => handleOrderSave()}
                       >
                         Add
