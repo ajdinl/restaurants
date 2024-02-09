652995e90ac95dd0b6ccd5e49f21b9028d617f42
ajdinl <ajdinl@live.com>
2024-02-09 15:28:17 +0100

    update content for dark mode

--- components/AdminDashboard.jsx
+++ components/AdminDashboard.jsx
@@ -57,7 +57,7 @@ export default function AdminDashboardComponent() {
 
   return (
     <div>
-      <main className='flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10'>
+      <main className='flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 bg-white dark:bg-gray-900 text-black dark:text-white'>
         {!view && (
           <div className='text-3xl text-center'>
             Welcome to the Admin Dashboard
