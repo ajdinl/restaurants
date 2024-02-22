c24e23a3256d13f21bcf2b46bf4a7f78e60b192f
ajdinl <ajdinl@live.com>
2024-02-22 14:48:13 +0100

    update delete modal

--- components/modals/DeleteModal.jsx
+++ components/modals/DeleteModal.jsx
@@ -30,18 +30,18 @@ export default function DeleteModal({
             </div>
             <div className='flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b'>
               <button
-                className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                className='text-green-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type='button'
                 onClick={() => setShowDeleteModal(false)}
               >
                 Close
               </button>
               <button
-                className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                className='bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type='button'
                 onClick={() => confirmDelete()}
               >
-                Save Changes
+                Delete
               </button>
             </div>
           </div>
